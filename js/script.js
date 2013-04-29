9ab1b726df0204b55c9ed64d3238e79167ab9e52
Lawrence Moore <lawrence2978@hotmail.com>
2013-04-28 20:28:39 -0500

    Created questions and added randomizing feature

--- js/script.js
+++ js/script.js
@@ -1,21 +1,58 @@
 $(document).ready(function(){
-	"use strict";
+	'use strict';
 	
-//Stores all of the quesitons	
-	var questions = [ 
-	"Who was Bilbo's newphes?", 
-	"What was the name of Frodo's best friend?", 
-	"Who was the bad guy?", 
-	"What was the name of the wizard?"
-	];
+	//Questions for the test
+	function nerdTest(){
+		
+		//Variables
+		var testQuestions, question, i;
+		
+		//For Array
+		i=0;
+		
+		//Test Questions
+		testQuestions = [ 
+		'Who was Bilbo\'s newphes?', 
+		'What was the name of Frodo\'s best friend?', 
+		'Who was the bad guy?', 
+		'What was the name of the wizard?'
+		];
+		
+		//Randomizes test questions
+		question = testQuestions.sort(function(){ 
+			return Math.random()-0.5; 
+		});
 	
-	questions.sort(function(){ 
-		return Math.random()-0.5; 
-	}); 
+		//Returns random question
+		return(question[i]);
+	} 
+
+	//Displays questions
+	function showQuestion(){
+		$('#testQuestion').text(nerdTest());
+	}
 	
-	console.log(questions);
+	//Calls function to display questions
+	showQuestion();
 });
 
 
 
 
+
+
+function testAnswers(){
+	'use strict';
+	
+	var answers1, answers2, answers3, answers4;
+	//Test Answer Arrays
+	answers1 = ['Frodo', 'Mordor', 'Lando', 'Beiber'];
+	answers2 = ['Sam', 'Tolkien', 'Boba', 'Batman'];
+	answers3 = ['Sauron', 'Borg', 'Darth Maul', 'LeBron'];
+	answers4 = ['Gandalf', 'Merlin', 'Yoda', 'David Blane'];
+}
+
+
+
+
+	
